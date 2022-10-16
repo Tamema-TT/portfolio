35e26e57fa8576a46ddd87caa780abc5cd20c624
Tamema-TT <tamema.tama@northsouth.edu>
2022-10-17 04:02:21 +0600

    Base Commit

--- js/tab-button.js
+++ js/tab-button.js
@@ -20,3 +20,28 @@ const tabActive = (content, tab) => {
     currentTab.classList.add('tab-active');
     // currentTab.classList.add('text-accent');
 }
+
+const sendEmail = () => {
+    let params = {
+        name: document.getElementById('name').value,
+        phone: document.getElementById('phone').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    }
+    const serviceId = "service_gulew9s";
+    const templateId = "template_w0a3f2e";
+    emailjs.send(serviceId, templateId, params)
+    .then(
+        res => {
+            document.getElementById('name').value = '';
+            document.getElementById('phone').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('subject').value = '';
+            document.getElementById('message').value = '';
+            console.log(res);
+            alert('successful');
+        }
+    )
+    .catch((err) => console.log(err));
+}
